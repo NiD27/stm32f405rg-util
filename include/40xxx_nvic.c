b672abbd730ab06fbf67cd1139035069828780b6
NiD27 <a.n.nidhish@gmail.com>
2023-02-21 04:51:48 +0000

    nvic

--- include/40xxx_nvic.c
+++ include/40xxx_nvic.c
@@ -1,1 +1,84 @@
 #include "40xxx_nvic.h"
+
+static NVIC_ISER_REGISTERS_ST *const NVIC_ISER = (NVIC_ISER_REGISTERS_ST *const)NVIC_ISER_START_ADDRESS;
+static NVIC_ICER_REGISTERS_ST *const NVIC_ICER = (NVIC_ICER_REGISTERS_ST *const)NVIC_ICER_START_ADDRESS;
+static NVIC_ISPR_REGISTERS_ST *const NVIC_ISPR = (NVIC_ISPR_REGISTERS_ST *const)NVIC_ISPR_START_ADDRESS;
+static NVIC_ICPR_REGISTERS_ST *const NVIC_ICPR = (NVIC_ICPR_REGISTERS_ST *const)NVIC_ICPR_START_ADDRESS;
+static NVIC_IABR_REGISTERS_ST *const NVIC_IABR = (NVIC_IABR_REGISTERS_ST *const)NVIC_IABR_START_ADDRESS;
+
+/*Returns 0 on failure 1 on success*/
+uint8_t nvic_iser_set_enable(uint8_t position){
+    uint8_t index = 0, bit_position = 0;
+
+    if(position < 0 || position > 239){
+        return 0;
+    }else{
+        index = (uint8_t)NVIC_ISER_INDEX_CALCULATOR(positon);
+        bit_position = (uint8_t)NVIC_ISER_BIT_POSITION_CALCULATOR(positon);
+        NVIC_ISER->ISER[index] = (NVIC_ISER->[index] | (0b1<<bit_position));
+        return 1;
+    }
+}
+
+uint8_t nvic_icer_set_enable(uint8_t position){
+    uint8_t index = 0, bit_position = 0;
+
+    if(position < 0 || position > 239){
+        return 0;
+    }else{
+        index = (uint8_t)NVIC_ICER_INDEX_CALCULATOR(positon);
+        bit_position = (uint8_t)NVIC_ICER_BIT_POSITION_CALCULATOR(positon);
+        NVIC_ICER->ICER[index] = (NVIC_ICER->[index] | (0b1<<bit_position));
+        return 1;
+    }
+}
+
+uint8_t nvic_ispr_set_enable(uint8_t position){
+    uint8_t index = 0, bit_position = 0;
+
+    if(position < 0 || position > 239){
+        return 0;
+    }else{
+        index = (uint8_t)NVIC_ISPR_INDEX_CALCULATOR(positon);
+        bit_position = (uint8_t)NVIC_ISPR_BIT_POSITION_CALCULATOR(positon);
+        NVIC_ISPR->ISPR[index] = (NVIC_ISPR->[index] | (0b1<<bit_position));
+        return 1;
+    }
+}
+
+uint8_t nvic_icpr_set_enable(uint8_t position){
+    uint8_t index = 0, bit_position = 0;
+
+    if(position < 0 || position > 239){
+        return 0;
+    }else{
+        index = (uint8_t)NVIC_ICPR_INDEX_CALCULATOR(positon);
+        bit_position = (uint8_t)NVIC_ICPR_BIT_POSITION_CALCULATOR(positon);
+        NVIC_ICPR->ICPR[index] = (NVIC_ICPR->[index] | (0b1<<bit_position));
+        return 1;
+    }
+}
+
+uint8_t nvic_iabr_set_enable(uint8_t position){
+    uint8_t index = 0, bit_position = 0;
+
+    if(position < 0 || position > 239){
+        return 0;
+    }else{
+        index = (uint8_t)NVIC_IABR_INDEX_CALCULATOR(positon);
+        bit_position = (uint8_t)NVIC_IABR_BIT_POSITION_CALCULATOR(positon);
+        NVIC_IABR->IABR[index] = (NVIC_IABR->[index] | (0b1<<bit_position));
+        return 1;
+    }
+}
+
+uint8_t nvic_ipr_set_priority(uint8_t position, uint8_t priority){
+    uint8_t index = 0, byte_position = 0;
+    if(position < 0 || positon > 239){
+        return 0;
+    }else{
+        index = (uint8_t)NVIC_IPR_INDEX_CALCULATOR(position);
+        byte_position = (uint8_t)NVIC_IPR_BYTE_POSITION_CALCULATOR(position);
+        NVIC_IPR->IPR[index].IP[byte_position] = priority;
+    }
+}
--- include/40xxx_nvic.h
+++ include/40xxx_nvic.h
@@ -9,9 +9,18 @@
 * Added some fool proofing, might backfire someday.
 */
 #define NVIC_ISER_OFFSET_CALCULATOR(x) (0x04*(x<0?0:x>7?7:x))
+/* No fool proofing divide at your own risk*/
+#define NVIC_ISER_INDEX_CALCULATOR(pos) (pos/32)
+#define NVIC_ISER_BIT_POSITION_CALCULATOR(pos) (pos%32)
 typedef struct{
     uint32_t SETENA :32;
 }NVIC_ISER_ST;
+/**sub super structure
+* I know comments are getting confusing
+*/
+typedef struct{
+    NVIC_ISER_ST ISER[8];
+}NIVC_ISER_REGISTERS_ST;
 
 #define NVIC_ICER_START_ADDRESS 0xe000e180U
 #define NVIC_ICER_END_ADDRESS 0xe000e19fU
@@ -21,9 +30,16 @@ typedef struct{
 * Added some fool proofing, might backfire someday.
 */
 #define NVIC_ICER_OFFSET_CALCULATOR(x) (0x04*(x<0?0:x>7?7:x))
+/* No fool proofing divide at your own risk*/
+#define NVIC_ICER_INDEX_CALCULATOR(pos) (pos/32)
+#define NVIC_ICER_BIT_POSITION_CALCULATOR(pos) (pos%32)
 typedef struct{
     uint32_t CLRENA :32;
 }NVIC_ICER_ST;
+/*sub super structure*/
+typedef struct{
+    NVIC_ICER_ST ICER[8];
+}NVIC_ICER_REGISTERS_ST;
 
 #define NVIC_ISPR_START_ADDRESS 0xe000e200U
 #define NVIC_ISPR_END_ADDRESS 0xe000e21fU
@@ -33,9 +49,16 @@ typedef struct{
 * Added some fool proofing, might backfire someday.
 */
 #define NVIC_ISPR_OFFSET_CALCULATOR(x) (0x04*(x<0?0:x>7?7:x))
+/* No fool proofing divide at your own risk*/
+#define NVIC_ISPR_INDEX_CALCULATOR(pos) (pos/32)
+#define NVIC_ISPR_BIT_POSITION_CALCULATOR(pos) (pos%32)
 typedef struct{
     uint32_t SETPEND :32;
 }NVIC_ISPR_ST;
+/*sub super structure*/
+typedef struct{
+    NVIC_ISPR_ST ISPR[8];
+}NVIC_ISPR_REGISTERS_ST;
 
 #define NVIC_ICPR_START_ADDRESS 0xe000e280U
 #define NVIC_ICPR_END_ADDRESS 0xe000e29fU
@@ -45,9 +68,16 @@ typedef struct{
 * Added some fool proofing, might backfire someday.
 */
 #define NVIC_ICPR_OFFSET_CALCULATOR(x) (0x04*(x<0?0:x>7?7:x))
+/* No fool proofing divide at your own risk*/
+#define NVIC_ICPR_INDEX_CALCULATOR(pos) (pos/32)
+#define NVIC_ICPR_BIT_POSITION_CALCULATOR(pos) (pos%32)
 typedef struct{
     uint32_t CLRPEND :32;
 }NVIC_ICPR_ST;
+/*sub super structure*/
+typedef struct{
+    NVIC_ICPR_ST ICPR[8];
+}NVIC_ICPR_REGISTERS_ST;
 
 #define NVIC_IABR_START_ADDRESS 0xe000e300U
 #define NVIC_IABR_END_ADDRESS 0xe000e31fU
@@ -57,9 +87,16 @@ typedef struct{
 * Added some fool proofing, might backfire someday.
 */
 #define NVIC_IABR_OFFSET_CALCULATOR(x) (0x04*(x<0?0:x>7?7:x))
+/* No fool proofing divide at your own risk*/
+#define NVIC_IABR_INDEX_CALCULATOR(pos) (pos/32)
+#define NVIC_IABR_BIT_POSITION_CALCULATOR(pos) (pos%32)
 typedef struct{
     uint32_t ACTIVE :32;
 }NVIC_IABR_ST;
+/*sub super structure*/
+typedef struct{
+    NVIC_IABR_ST IABR[8];
+}NVIC_IABR_REGISTERS_ST;
 
 #define NVIC_IPR_START_ADDRESS 0xe000e400U
 #define NVIC_IPR_END_ADDRESS 0xe000e29fU
@@ -79,6 +116,10 @@ typedef struct{
 typedef struct{
     uint8_t IP[4];
 }NVIC_IPR_ST;
+/*sub super structure*/
+typedef struct{
+    NVIC_IPR_ST IPR[60];
+}NVIC_IPR_REGISTERS_ST;
 
 #define NVIC_STIR_START_ADDRESS 0xe000ef00U
 #define NVIC_STIR_SIZE 0x04U
@@ -86,5 +127,9 @@ typedef struct{
     uint32_t INTID :9;
     uint32_t RESERVED :23;
 }NVIC_STIR_ST;
-
+/**NVIC_STIR_ST is a single register, doesnt need a 
+* sub super struct. Should I just typedef NVIC_STIR_ST as 
+* NVIC_STIR_REGISTERS_ST?
+* To do or not to do.
+*/
 #endif
